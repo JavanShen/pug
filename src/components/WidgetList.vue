1fc85751f4f317322c498a4fd7e1ca266bf9bf5f
JavanShen <1985486089@qq.com>
2022-07-07 15:04:39 +0800

    :sparkles: 左侧组件列表

---
+++ src/components/WidgetList.vue
@@ -0,0 +1,23 @@
+<template>
+    <Preview :overview="list" />
+</template>
+
+<script setup>
+import Preview from '@/render/preview.vue'
+
+const elements = ['input', 'button'].map(item => {
+    return {
+        tag: item,
+        span: 12,
+        content: 'init'
+    }
+})
+
+const list = {
+    draggable: {
+        open: true,
+        source: true  
+    },
+    elements
+}
+</script>
