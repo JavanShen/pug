946a2eb25a6e58267e8857037887a5d2a99411ad
JavanShen <1985486089@qq.com>
2022-07-07 15:03:49 +0800

    :hammer: 根据json配置返回对应组件

---
+++ src/render/JsonToComponent.js
@@ -0,0 +1,15 @@
+import getComponent from './components'
+
+export default function JsonToComponent(obj = {}) {
+    const { tag, content, ...other } = obj
+
+    const Tag = getComponent(tag)
+
+    return {
+        render() {
+            return Tag ? (
+                <Tag {...other}>{content}</Tag>
+            ) : (<div></div>)
+        }
+    }
+}
