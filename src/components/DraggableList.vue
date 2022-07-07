92c061eab99f7c1c943eb0eb95adad675ad76721
JavanShen <1985486089@qq.com>
2022-07-07 15:02:07 +0800

    :alembic: vue-dragable组件拖拽测试

---
+++ src/components/DraggableList.vue
@@ -0,0 +1,76 @@
+<template>
+    <fieldset name="one">
+        <legend>one</legend>
+        <draggable tag="ul" :component-data="{
+            type: 'transition-group',
+            name: 'flip-list'
+        }" v-bind="dragOptions({ group: { name: 'test', pull: 'clone', put: false } })" :list="list1" item-key="order"
+            @start="start" @end="end">
+            <template #item="{ element }">
+                <li class="list-group-item">{{ element.name }}</li>
+            </template>
+        </draggable>
+    </fieldset>
+    <fieldset name="two">
+        <legend>two</legend>
+        <draggable tag="ul" :component-data="{
+            type: 'transition-group',
+            name: 'flip-list'
+        }" v-bind="dragOptions()" :list="list2" item-key="order" @start="start" @end="end">
+            <template #item="{ element }">
+                <li class="list-group-item">{{ element.name }}</li>
+            </template>
+        </draggable>
+    </fieldset>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import draggable from 'vuedraggable'
+
+const dragOptions = (obj) => ({
+    animation: 200,
+    group: 'test',
+    disabled: false,
+    ghostClass: 'ghost',
+    ...obj
+})
+
+const data1 = ['dog', 'cat', 'bird', 'horse', 'panda']
+const data2 = ['apple', 'banana']
+
+const list1 = ref(generateList(data1))
+
+const list2 = ref(generateList(data2))
+
+const start = () => {
+    console.log('start')
+}
+
+const end = () => {
+    console.log('end')
+}
+
+function generateList(data) {
+    return data.map((name, index) => {
+        return {
+            name,
+            order: index + 1
+        }
+    })
+}
+</script>
+
+<style scoped>
+.ghost {
+    opacity: 0.5;
+}
+
+.flip-list-move {
+    transition: transform 0.5s;
+}
+
+.list-group-item {
+    cursor: move;
+}
+</style>
