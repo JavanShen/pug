1364e243ba53a9027ea451995fff6c2d04911d6a
JavanShen <1985486089@qq.com>
2022-07-07 15:05:51 +0800

    :construction: 主体页面

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <n-layout has-sider>
+        <n-layout-sider content-style="padding: 24px;">
+            <WidgetList />
+        </n-layout-sider>
+        <n-layout>
+            <n-layout-header>颐和园路</n-layout-header>
+            <n-layout-content content-style="padding: 24px;">
+                <TestForm />
+            </n-layout-content>
+        </n-layout>
+    </n-layout>
+</template>
+
+<script setup>
+import { NLayout, NLayoutHeader, NLayoutContent, NLayoutSider } from 'naive-ui'
+import WidgetList from '@/components/WidgetList.vue'
+</script>
