f1ec6a9267634d51fd266681efe8e8dfa7533227
JavanShen <1985486089@qq.com>
2022-07-14 00:13:33 +0800

    :hammer: 新增自定义标签组件

---
+++ src/render/components/Tag.js
@@ -0,0 +1,25 @@
+import { NTag } from 'naive-ui'
+import { toRefs } from 'vue'
+
+export default {
+    name: 'tag',
+    props: {
+        value: {
+            type: String,
+            default: '无名'
+        },
+        icon: {
+            type: String,
+            default: 'input'
+        }
+    },
+    setup(props) {
+        const { value, icon } = toRefs(props)
+
+        const slot = {
+            icon: () => <svg-icon iconClass={icon.value} />
+        }
+
+        return () => <NTag v-slots={slot}>{value.value}</NTag>
+    }
+}
