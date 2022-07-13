904e8a8166ca9a6cad661979d22a65a9f4c1624b
JavanShen <1985486089@qq.com>
2022-07-14 00:26:41 +0800

    :lipstick: 设置宽高,添加边线

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
-    <n-layout has-sider>
-        <n-layout-sider content-style="padding: 24px;">
+    <n-layout class="container" has-sider>
+        <n-layout-sider bordered content-style="padding: 24px;">
             <WidgetList />
         </n-layout-sider>
         <n-layout>
-            <n-layout-header>颐和园路</n-layout-header>
-            <n-layout-content content-style="padding: 24px;"> 新北路 </n-layout-content>
+            <n-layout-header bordered class="header"
+                >颐和园路<svg-icon icon-class="tag"
+            /></n-layout-header>
+            <n-layout-content class="content" content-style="padding: 24px;">
+                <WorkSpace />
+            </n-layout-content>
         </n-layout>
     </n-layout>
 </template>
@@ -13,4 +17,21 @@
 <script setup>
 import { NLayout, NLayoutHeader, NLayoutContent, NLayoutSider } from 'naive-ui'
 import WidgetList from '@/components/WidgetList.vue'
+import WorkSpace from '@/components/WorkSpace.vue'
+
+const headerHeight = `${60}px`
 </script>
+
+<style scoped>
+.container {
+    height: 100vh;
+}
+
+.header {
+    height: v-bind(headerHeight);
+}
+
+.content {
+    height: calc(100vh - v-bind(headerHeight));
+}
+</style>
