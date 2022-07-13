4ceffd2b862c5ac12577cc0fc6cba267e3e55147
JavanShen <1985486089@qq.com>
2022-07-13 23:56:44 +0800

    :sparkles: 新增svg雪碧图

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import { createApp } from 'vue'
 import { create, NGrid } from 'naive-ui'
+import SvgIcon from '@/icon'
 import App from './App.vue'
 
 const naiveui = create({
     components: [NGrid]
 })
 
-createApp(App).use(naiveui).mount('#app')
+createApp(App).use(SvgIcon).use(naiveui).mount('#app')
