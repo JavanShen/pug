719991618c376b1aea525d3c722db3159e881874
JavanShen <1985486089@qq.com>
2022-07-14 00:25:36 +0800

    :sparkles: 主编辑页面

---
+++ src/components/WorkSpace.vue
@@ -0,0 +1,31 @@
+<template>
+    <Preview class="work-space" :overview="finalConfig" />
+</template>
+
+<script setup>
+import Preview from '@/render/preview.vue'
+import { computed } from 'vue'
+
+const props = defineProps({
+    config: {
+        type: Object,
+        default: () => ({ elements: [] }),
+        require: true
+    }
+})
+
+const finalConfig = computed(() => {
+    return {
+        draggable: {
+            group: 'creation'
+        },
+        ...props.config
+    }
+})
+</script>
+
+<style scoped>
+.work-space {
+    height: 100%;
+}
+</style>
