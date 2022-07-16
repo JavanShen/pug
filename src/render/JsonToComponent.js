f0a90899694b7636b85c04cafcea35a0bd0c344e
JavanShen <1985486089@qq.com>
2022-07-16 16:45:04 +0800

    :hammer: 增加虚拟dom支持

--- src/render/JsonToComponent.js
+++ src/render/JsonToComponent.js
@@ -1,7 +1,10 @@
+import { isVNode } from 'vue'
 import getComponent from './components'
 
 export default function JsonToComponent(obj = {}) {
     const { tag, content, ...other } = obj
+
+    if (isVNode(tag)) return tag
 
     const Tag = getComponent(tag) || tag
 
