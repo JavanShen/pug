da78a6cb7cee86a3f58d1282cbbbfe9575090019
JavanShen <1985486089@qq.com>
2022-08-02 13:53:25 +0800

    :sparkles: 添加右侧设置栏

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-    <n-layout class="container" has-sider>
-        <n-layout-sider bordered content-style="padding: 24px;">
-            <WidgetList />
-        </n-layout-sider>
-        <n-layout>
-            <n-layout-header bordered class="header"
-                >颐和园路<svg-icon icon-class="tag"
-            /></n-layout-header>
+    <n-layout class="container">
+        <n-layout-header bordered class="header">颐和园路</n-layout-header>
+        <n-layout has-sider>
+            <n-layout-sider bordered content-style="padding: 24px;">
+                <WidgetList />
+            </n-layout-sider>
             <n-layout-content class="content" content-style="padding: 24px;">
                 <WorkSpace :config="config" @change="addWidget" />
             </n-layout-content>
+            <n-layout-sider class="right" content-style="padding: 24px;">
+                <RightPanel />
+            </n-layout-sider>
         </n-layout>
     </n-layout>
 </template>
@@ -19,6 +20,7 @@ import { ref } from 'vue'
 import { NLayout, NLayoutHeader, NLayoutContent, NLayoutSider } from 'naive-ui'
 import WidgetList from '@/components/WidgetList/index.vue'
 import WorkSpace from '@/components/WorkSpace.vue'
+import RightPanel from '@/components/RightPanel/index.vue'
 import componentsOption from '@/options'
 
 const headerHeight = `${60}px`
@@ -47,5 +49,16 @@ const addWidget = (to, from, widget) => {
 
 .content {
     height: calc(100vh - v-bind(headerHeight));
+}
+
+.right::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    width: 1px;
+    background-color: rgb(239, 239, 245);
+    transition: background-color 0.3s cubic-bezier(0.4, 0, 0.2, 1);
 }
 </style>
---
+++ src/components/RightPanel/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <n-tabs animated default-value="component">
+        <n-tab-pane name="component" tab="组件设置">组件设置</n-tab-pane>
+        <n-tab-pane name="form" tab="表单设置">表单设置</n-tab-pane>
+    </n-tabs>
+</template>
+
+<script setup>
+import { NTabs, NTabPane } from 'naive-ui'
+</script>
